05b7d942fe15d5848b347b7bac3d7fc5a6625dd1
Etienne Audouin <eaudouin@maincare.fr>
2018-07-26 14:46:13 +0200

    master: +script pour extraire des données

--- banguha/snippets.sh
+++ banguha/snippets.sh
@@ -1,2 +1,8 @@
 # extract table row
-cat melbi | sed 's/<div class="tableRow/\n<div class="tableRow/g' | grep tableRow | sed 's/<\/div><\/div><\/div><\/div>.*/<\/div><\/div>/g' 
+cat melbi | sed 's/<div class="tableRow/\n<div class="tableRow/g' | grep tableRow | sed 's/<\/div><\/div><\/div><\/div>.*/<\/div><\/div>/g' > melbi.html 
+
+# force line break every tag
+cat melbi.html | sed 's/>/>\n/g; s/</\n</g' | egrep -v '^\s*$' > melbi.breaked.html
+
+# extract strings
+cat melbi.breaked.html | grep -v '<.*>' > melbi.strings
