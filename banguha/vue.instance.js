8893419c782da4c9e574983e9be4dea3702d487a
phreno <losgoomy@gmail.com>
2018-07-29 16:10:47 +0200

    master: Affichage des traductions sur la page

--- banguha/vue.instance.js
+++ banguha/vue.instance.js
@@ -10,31 +10,70 @@ new Vue({
       waitForInput: undefined,
       waitingTime: 1000
     },
+    response: {
+      label: "Votre réponse",
+      placeholder: "...",
+      value: undefined,
+    },
+    data: {
+      translate: undefined,
+      translationMemory: undefined
+    }
   },
   watch:  {
     "reference.value" () {
-      function doReferenceUpdate() {
-        glosbeService.translate(instance.reference.value, data => console.log(data))
-        glosbeService.getTranslationMemory(instance.reference.value, data => console.log(data))
+      function doServiceCall() {
+        glosbeService.translate(instance.reference.value, instance.setDataTranslate)
+        glosbeService.getTranslationMemory(instance.reference.value, instance.setDataTranslationMemory)
         instance.waitForInput = undefined
       }
 
-      function preventUselessCall() {
+      function preventUselessCallToService() {
         if (instance.waitForInput) {
           clearTimeout(instance.waitForInput)
         }
       }
 
-      function waitAndDoUpdate() {
-        instance.waitForInput = setTimeout(doReferenceUpdate, 1000)
+      function waitAndDoServiceCall() {
+        instance.waitForInput = setTimeout(doServiceCall, 1000)
+      }
+
+      function handleReferenceChange() {
+        const noLetters = /^\W*$/
+        const noReference = instance.reference.value.match(noLetters)
+        if (noReference) {
+          instance.emptyData()
+        } else {
+          waitAndDoServiceCall()
+        }
       }
 
       const instance = this
-      preventUselessCall()
-      waitAndDoUpdate()
+      preventUselessCallToService()
+      handleReferenceChange()
+    }
+  },
+  computed: {
+    displayTraductions() {
+      return this.data && this.data.translationMemory && this.data.translationMemory.examples && this.data.translationMemory.examples.length > 0
     }
   },
   methods: {
-
+    setDataTranslate(data) {
+      this.data.translate = data
+    },
+    setDataTranslationMemory(data) {
+      this.data.translationMemory = data
+    },
+    emptyDataTranslate() {
+      this.setDataTranslate(undefined)
+    },
+    emptyDataTranslationMemory() {
+      this.setDataTranslationMemory(undefined)
+    },
+    emptyData() {
+      this.emptyDataTranslate()
+      this.emptyDataTranslationMemory()
+    }
   }
 })
